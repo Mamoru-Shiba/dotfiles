43fbcb600cd18b962a3ea589ed14134c80b42258
shiba <shiba@PC-adminnoMacBook-Pro.local>
2023-07-10 00:15:41 +0900

    add

--- .bin/.config/nvim/lua/base.lua
+++ .bin/.config/nvim/lua/base.lua
@@ -5,6 +5,6 @@ vim.scriptencoding = "utf-8"
 vim.wo.number = true
 
 -- Open hoge file
-vim.api.nvim_create_user_command("Hoge", function(opts)
-	vim.cmd("e " .. "~/_/hoge/hoge.markdown")
+vim.api.nvim_create_user_command("Memo", function(opts)
+	vim.cmd("e " .. "~/_/memo/memo.markdown")
 end, {})
